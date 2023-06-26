93f721ffbabb1dce49aa6459819f650a29327cbd
H_Voronkova <gallinavoronkova@gmail.com>
2023-06-26 21:27:25 +0200

    Added editing and deletion address

--- src/test/java/e2e/UserCanWorkWithAddressInNewContact.java
+++ src/test/java/e2e/UserCanWorkWithAddressInNewContact.java
@@ -10,6 +10,10 @@ import org.skyscreamer.jsonassert.JSONAssert;
 import org.skyscreamer.jsonassert.JSONCompareMode;
 import org.testng.Assert;
 import org.testng.annotations.Test;
+import schemas.AddressDto;
+
+import java.util.LinkedHashMap;
+import java.util.Map;
 
 public class UserCanWorkWithAddressInNewContact {
     Contact contact;
@@ -18,18 +22,18 @@ public class UserCanWorkWithAddressInNewContact {
     @Test
     public void userCanWorkWithAddressInNewContact() throws JSONException {
         contact = new Contact();
-        //create new contact TODO: POST
+        //create new contact - POST-запрос
         JsonPath createdContact = contact.createContact(201).jsonPath();
         int contactId = createdContact.getInt("id");        //чтобы вытащить из респонса наш id (value по ключу)
 
         address = new Address();
         address.createAddress(201, contactId);
         JsonPath createdAddress = address.getAllAddresses(200, contactId).jsonPath();
-        int addressId = createdAddress. getInt("[0].id");
-        String city = createdAddress. getString("[0].city");
-        String country = createdAddress. getString("[0].country");
-        String street = createdAddress. getString("[0].street");
-        String zip = createdAddress. getString("[0].zip");
+        int addressId = createdAddress.getInt("[0].id");
+        String city = createdAddress.getString("[0].city");
+        String country = createdAddress.getString("[0].country");
+        String street = createdAddress.getString("[0].street");
+        String zip = createdAddress.getString("[0].zip");
 
         JSONObject actualAddressJson = new JSONObject();
         actualAddressJson.put("id", addressId);
@@ -41,13 +45,40 @@ public class UserCanWorkWithAddressInNewContact {
 
         String actualJson = actualAddressJson.toString();
         String expectedJson = "{\n" +
-                "        \"id\": "+addressId+",\n" +
+                "        \"id\": " + addressId + ",\n" +
                 "        \"city\": \"Berlin\",\n" +
                 "        \"country\": \"Germany\",\n" +
                 "        \"street\": \"Thorner\",\n" +
                 "        \"zip\": \"21335\",\n" +
-                "        \"contactId\": "+contactId+"\n" +
+                "        \"contactId\": " + contactId + "\n" +
                 "    }";
         JSONAssert.assertEquals(actualJson, expectedJson, JSONCompareMode.STRICT);
+
+
+        //Add edit address
+        address.editAddress(200, addressId, contactId);
+        JsonPath editedAddress = address.getAddress(200, addressId).jsonPath();
+
+        LinkedHashMap<String, String> objectEditedData = new LinkedHashMap<>();
+        objectEditedData.put(editedAddress.getString("city"), address.dataForEditAddress(addressId, contactId).getCity());
+        objectEditedData.put(editedAddress.getString("country"), address.dataForEditAddress(addressId, contactId).getCountry());
+        objectEditedData.put(editedAddress.getString("street"), address.dataForEditAddress(addressId, contactId).getStreet());
+        objectEditedData.put(editedAddress.getString("zip"), address.dataForEditAddress(addressId, contactId).getZip());
+
+        //Add check that address was edited
+        for(Map.Entry<String, String> object: objectEditedData.entrySet()) {
+            String actualResult = object.getKey();
+            String expectedResult = object.getValue();
+            Assert.assertEquals(actualResult, expectedResult, actualResult + "not equals" + expectedResult);
+        }
+
+
+        //Add delete address
+        address.deleteAddress(200, addressId);
+
+        //Add check that address was deleted
+        JsonPath actualDeletedAddress = address.getAddress(500, addressId).jsonPath();
+        Assert.assertEquals(actualDeletedAddress.getString("message"), "Error! This address doesn't exist in our DB");
     }
+
 }
